52868ec331b0cbc9cddb4bd970e1b77989a7f2bc
JSousaLAB <jsalabsn@gmail.com>
2026-01-02 11:26:29 +0100

    NewPSFile
    Get Open PS ISE Files

---
+++ ListOpenPSISE.ps1
@@ -0,0 +1,17 @@
+#
+#
+#
+$Path = $PSScriptRoot
+#
+#$Path = $MyScriptPath
+#
+$CurrentDate = (Get-Date).ToString('ddMMyy_HHmm')
+#
+$PSFile = $Path+"\Log\PSFile"+$CurrentDate+".txt"
+#
+$psISE.PowerShellTabs.files.FullPath | Out-File -FilePath $PSFile
+#
+Start-Process notepad++ $PSFile
+#
+#
+#
